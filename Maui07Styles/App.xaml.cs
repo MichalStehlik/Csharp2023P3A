17d8199b7a71e5714be4fce7d0796ffc4f1e714f
Michal Stehlík <michal.stehlik@pslib.cz>
2023-12-06 10:10:30 +0100

    styles

---
+++ Maui07Styles/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui07Styles
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui07Styles/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui07Styles
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
